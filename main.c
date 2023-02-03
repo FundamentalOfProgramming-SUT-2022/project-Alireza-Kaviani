7212422d4bf1aed008ba0732bdaf703dc8d30e71
Alireza <alireza.kaviani.2005@gmail.com>
2023-02-03 16:12:41 +0330

    Phase1 completed

--- main.c
+++ main.c
@@ -1,24 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <sys/stat.h>
-#include "src/vecstr.h"
 #include "src/io.h"
 #include "src/file.h"
-#include "src/command.h"
-#include "src/commands/createfile.h"
-#include "src/undo.h"
-#include "src/commands/tree.h"
 
 int main(){
-    //string* path = create_file(char_to_str(OUTPUT), 1);
-
-    //FILE* f = fopen(path->s, "w");
+    mkdir(ROOT, 0777);
     while(!read_command(stdout));
-
-    /*fclose(f);
-    f = fopen(path->s, "r");
-    print_file(stdout, f);
-    fclose(f);*/
 
     return 0;
 }
